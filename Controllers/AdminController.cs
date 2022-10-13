bf57d07070f5292463e45ed3e19c625312fd6e5c
basharat-khan <bashartk189@gmail.com>
2022-10-13 12:28:10 +0530

    Incomplete Code in EditUsers

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -13,14 +13,14 @@ using System.Threading.Tasks;
 
 namespace EF_DotNetCore.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
 
         public readonly UserManager<ApplicaitonUsers> usermanager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager,UserManager<ApplicaitonUsers> userManager )
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicaitonUsers> userManager)
         {
             this.roleManager = roleManager;
             usermanager = userManager;
@@ -40,7 +40,7 @@ namespace EF_DotNetCore.Controllers
         }
 
         [HttpPost]
-        
+
         public async Task<IActionResult> CreateRole(CreateRoleViewModel obj)
         {
             if (ModelState.IsValid)
@@ -52,22 +52,22 @@ namespace EF_DotNetCore.Controllers
                 var role = await roleManager.CreateAsync(identityRole);
                 if (role.Succeeded)
                 {
-                    return RedirectToAction("GetRoles","Admin");
+                    return RedirectToAction("GetRoles", "Admin");
                 }
                 else
                 {
-                    foreach(var a in role.Errors)
+                    foreach (var a in role.Errors)
                     {
                         ModelState.AddModelError(string.Empty, a.Description);
                     }
-                    
+
                 }
             }
             return View(obj);
         }
 
         [HttpGet]
-        
+
         public async Task<IActionResult> RoleEditView(string ID)
         {
             var role = await roleManager.FindByIdAsync(ID);
@@ -91,17 +91,17 @@ namespace EF_DotNetCore.Controllers
                 ViewBag.ErrorMsg = $"Role witth ID: {ID} not found";
                 return View("ViewNotFound");
             }
-            }
+        }
 
         [HttpPost]
-        
+
         public async Task<IActionResult> RoleEditView(RoleEditModel model)
         {
-            var role=await roleManager.FindByIdAsync(model.Id);
-            if(role != null)
+            var role = await roleManager.FindByIdAsync(model.Id);
+            if (role != null)
             {
                 role.Name = model.RoleName;
-                var result=await roleManager.UpdateAsync(role);
+                var result = await roleManager.UpdateAsync(role);
 
                 if (result.Succeeded)
                 {
@@ -126,23 +126,23 @@ namespace EF_DotNetCore.Controllers
         [HttpGet]
         public async Task<IActionResult> EditUserInRole(string ID)
         {
-            ViewBag.RoleId = ID;    
-            var role=await roleManager.FindByIdAsync(ID);
-            if(role==null)
+            ViewBag.RoleId = ID;
+            var role = await roleManager.FindByIdAsync(ID);
+            if (role == null)
             {
                 ViewBag.ErrorMsg = $"The Role wiht ID:{ID} not found";
                 return View("ViewNotFound");
             }
             else
             {
-                var model =new List<UserRoleViewModel>();
+                var model = new List<UserRoleViewModel>();
 
                 foreach (var item in usermanager.Users.ToList())
                 {
                     var a = new UserRoleViewModel();
                     a.UserName = item.UserName;
                     a.UserID = item.Id;
-                    if(await usermanager.IsInRoleAsync(item, role.Name))
+                    if (await usermanager.IsInRoleAsync(item, role.Name))
                     {
                         a.IsSelected = true;
                     }
@@ -158,10 +158,10 @@ namespace EF_DotNetCore.Controllers
 
         [HttpPost]
         [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> EditUserInRole(List<UserRoleViewModel> list,string ID)
+        public async Task<IActionResult> EditUserInRole(List<UserRoleViewModel> list, string ID)
         {
-           var role = await roleManager.FindByIdAsync(ID);
-            if(role == null)
+            var role = await roleManager.FindByIdAsync(ID);
+            if (role == null)
             {
                 ViewBag.ErrorMsg = $"Role with ID:{ID} does not exists";
                 return View("ViewNotFound");
@@ -170,21 +170,21 @@ namespace EF_DotNetCore.Controllers
             {
                 for (int i = 0; i < list.Count; i++)
                 {
-                    var user =await usermanager.FindByIdAsync(list[i].UserID);
+                    var user = await usermanager.FindByIdAsync(list[i].UserID);
                     IdentityResult result = null;
-                    if (list[i].IsSelected && !(await usermanager.IsInRoleAsync(user,role.Name)))
+                    if (list[i].IsSelected && !(await usermanager.IsInRoleAsync(user, role.Name)))
                     {
                         result = await usermanager.AddToRoleAsync(user, role.Name);
                     }
-                    else if (!list[i].IsSelected && (await usermanager.IsInRoleAsync(user,role.Name)))
+                    else if (!list[i].IsSelected && (await usermanager.IsInRoleAsync(user, role.Name)))
                     {
-                        result = await usermanager.RemoveFromRoleAsync(user, role.Name);                    
+                        result = await usermanager.RemoveFromRoleAsync(user, role.Name);
                     }
                     else
                     {
                         continue;
                     }
-                    if(result.Succeeded)
+                    if (result.Succeeded)
                     {
                         if (i < list.Count - 1)
                         {
@@ -192,18 +192,40 @@ namespace EF_DotNetCore.Controllers
                         }
                         else
                         {
-                            return RedirectToAction("RoleEditView",new {id=ID});
+                            return RedirectToAction("RoleEditView", new { id = ID });
                         }
                     }
                 }
-                return RedirectToAction("RoleEditView", new {id=ID});
+                return RedirectToAction("RoleEditView", new { id = ID });
             }
         }
+
+        [HttpGet]
         public IActionResult ListOfUsers()
         {
 
-            var userModel =usermanager.Users;
+            var userModel = usermanager.Users;
             return View(userModel);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user =await usermanager.FindByIdAsync(id);
+            if(user == null)
+            {
+                ViewBag.ErrorMsg = $"User with ID: {id} not found";
+                return View("ViewNotFound");
+            }
+            else
+            {
+                var claim=await usermanager.GetClaimsAsync(user);
+                var role = await usermanager.GetRolesAsync(user);
+                EditUser a = new EditUser();
+
+                return View(user);
+            }
+        }
+
     }
 }
---
+++ Models/EditUserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EF_DotNetCore.Models
+{
+    public class EditUserViewModel
+    {
+        public string ID { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public Genders gender { get; set; }
+        public List<string> Roles { get; set; }
+
+        public List<string> Claims { get; set; }
+    }
+}
